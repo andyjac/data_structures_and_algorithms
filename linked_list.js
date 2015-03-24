11df6fcf8ef95ad076a3d2967f262ad5c5218c43
Andrew <andyjacuw@gmail.com>
2015-03-23 23:24:42 -0700

    Add implementation of a linked list

---
+++ linked_list.js
@@ -0,0 +1,61 @@
+var LList = (function() {
+
+  function LList() {
+    this.head = new Node('head');
+  }
+
+  LList.prototype.insert = function(newElement, item) {
+    var newNode = new Node(newElement);
+    var current = find.call(this, item);
+    newNode.next = current.next;
+    current.next = newNode;
+  };
+
+  LList.prototype.display = function() {
+    var currNode = this.head;
+
+    while (!(currNode.next === null)) {
+      console.log(currNode.next.element);
+      currNode = currNode.next;
+    }
+  };
+
+  LList.prototype.remove = function(item) {
+    var prevNode = findPrevious.call(this, item);
+
+    if (!(prevNode.next === null)) {
+      prevNode.next = prevNode.next.next;
+    }
+  };
+
+  function find(item) {
+    var currNode = this.head;
+
+    while (currNode.element !== item) {
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+
+  function findPrevious(item) {
+    var currNode = this.head;
+
+    while (!(currNode.next === null) &&
+           (currNode.next.element !== item)) {
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+
+  return LList;
+})();
+
+var Node = (function() {
+
+  function Node(element) {
+    this.element = element;
+    this.next = null;
+  }
+
+  return Node;
+})();
