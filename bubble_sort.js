3e6fc06d5f23e2f12fdce7808d2f4321d9462a55
Andrew Jacobson <andyjacuw@gmail.com>
2015-04-17 14:39:52 -0700

    Add bubble sort algorithm

---
+++ bubble_sort.js
@@ -0,0 +1,17 @@
+function bubbleSort(arr) {
+  var numElements = arr.length, temp;
+
+  for (var outer = numElements; outer >= 2; --outer) {
+    for (var inner = 0; inner <= outer - 1; ++inner) {
+      if (arr[inner] > arr[inner + 1]) {
+        swap(arr, inner, inner + 1);
+      }
+    }
+  }
+}
+
+function swap(arr, index1, index2) {
+  var temp = arr[index1];
+  arr[index1] = arr[index2];
+  arr[index2] = temp;
+}
